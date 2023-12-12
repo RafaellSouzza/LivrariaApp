c50771efbaf9172f0827ff20c72807bf6c41b6fb
Rafa Souza <31011616+RafaellSouzza@users.noreply.github.com>
2023-12-12 04:55:15 -0300

    Criação do projeto

---
+++ LivrariaApp/src/main/java/br/edu/infnet/LivrariaApp/LivrariaAppApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.LivrariaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaAppApplication.class, args);
+	}
+
+}
